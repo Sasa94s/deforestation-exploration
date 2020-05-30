ddb335930874d9b7d2eec9362ddb496c88422889
Mostafa Elsheikh <abdelaleem@gmx.com>
2020-05-31 01:19:26 +0200

    feat: add PDF report, DB View, notebook source code

---
+++ create_views.sql
@@ -0,0 +1,13 @@
+CREATE OR REPLACE VIEW forestation AS
+    SELECT r.country_code,
+       r.country_name,
+       r.region,
+       r.income_group,
+       fa.year,
+       fa.forest_area_sqkm,
+       la.total_area_sq_mi * 2.59 AS total_area_sqkm,
+       ROUND(fa.forest_area_sqkm / (la.total_area_sq_mi * 2.59) * 100, 2) AS forest_percentage
+FROM regions r
+JOIN forest_area fa ON r.country_code = fa.country_code
+JOIN land_area la ON fa.country_code = la.country_code AND
+                     fa.year = la.year;
--- init_db.sh
+++ init_db.sh
@@ -6,3 +6,4 @@ PATH=$(pwd)
 /usr/local/bin/psql -d forest_query -c "COPY forest_area FROM '${PATH}/data/forest_area.csv' DELIMITER ',' CSV HEADER;"
 /usr/local/bin/psql -d forest_query -c "COPY land_area FROM '${PATH}/data/land_area.csv' DELIMITER ',' CSV HEADER;"
 /usr/local/bin/psql -d forest_query -c "COPY regions FROM '${PATH}/data/regions.csv' DELIMITER ',' CSV HEADER;"
+/usr/local/bin/psql -d forest_query -f create_views.sql
